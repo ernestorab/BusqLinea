74434810b987d71ae79915c62403927d72c9b2e1
Ernesto Javier Ramírez Abitia <ernestorab@gmail.com>
2014-02-05 20:42:25 -0600

    Termina el ciclo si t<0.1

--- BacktrakingInterpolacion2.m
+++ BacktrakingInterpolacion2.m
@@ -6,13 +6,16 @@ gprima0=g'*p;
 g1=feval(fname,x+t*p);
 
 while(g1 > g0 + t*c1*gprima0)
-
+    
     g1=feval(fname,x+t*p);
     t=-(gprima0*t^2) /(2*(g1-t*gprima0-g0));
+    
+    if t<0.1
+        t=0.1;
+        break;
+    end
 
 end
 
-if t<0.1
-    t=0.1;
-end
 
+end
