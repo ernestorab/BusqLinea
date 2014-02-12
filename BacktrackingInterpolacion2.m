fa448f52274f003f31c45b8dd50773ae8c7debf2
mophicot <srangelor@gmail.com>
2014-02-11 18:14:58 -0600

    Update BacktrackingInterpolacion2.m

--- BacktrackingInterpolacion2.m
+++ BacktrackingInterpolacion2.m
@@ -1,11 +1,22 @@
 function [ t ] = BacktrakingInterpolacion2( fname,g,x,c1,p )
-%Método de Backtracking por el método de Backtracking
+%Método de Backtracking usando Interpolación para obtener la t que cumpla la condición de Wolfe
+
+%In.-
+% fname; cadena de caracteres con el nombre de la funcion a minimizar
+% g; vector grafiente de la función fname de dimensión n
+%x; vector n dimensional que denota el punto inicial
+%c1; es el parametro relacionado a la primera condición de Wolfe
+% p es un vector n dimensional, es la dirección de descenso para la busqueda de línea
+
+%Out
+% t .- la t que cumple la condición de Wolfe 
+
 %DESCRIPCIÓN
 
-t=1;
+t=1; % valor incial de t
 g0=feval(fname, x);     %función evaluada
 gprima0=g'*p;
-g1=feval(fname,x+t*p);
+g1=feval(fname,x+t*p);  % función evaluada al dar el paso completo
 
 while(g1 > g0 + t*c1*gprima0)
     
