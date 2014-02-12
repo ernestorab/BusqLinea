463570aa623a1faa9455787f05ef67fab5071589
Ernesto Javier Ramírez Abitia <ernestorab@gmail.com>
2014-02-12 12:22:27 -0600

    Update BLEasom.m

--- BLEasom.m
+++ BLEasom.m
@@ -1,7 +1,7 @@
 [x, iter] = metodoBL('Easom',[5;5],'M','R');
-display('El punto en donde la función de Rosenbrock alcanza el mínimo es ')
+display('El punto en donde la función de Easom alcanza el mínimo es ')
 x
-display('El valor de la función de Rosenbrock es ese punto es ')
+display('El valor de la función de Easom es ese punto es ')
 rosenbrock(x)
 display('Este punto se alcanzó en ')
 iter
