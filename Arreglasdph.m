db7a17309a9f525d2538a0d591121607f84bfdd0
Ernesto Javier Ramírez Abitia <ernestorab@gmail.com>
2014-02-11 19:26:18 -0600

    Update Arreglasdph.m

--- Arreglasdph.m
+++ Arreglasdph.m
@@ -2,17 +2,20 @@ function [ H ] =Arreglasdph( H )
 %
 %IN matriz hessiana
 %
-%Out matriz hessiana definida positiva
+%OUT matriz hessiana definida positiva
 
 %valores iniciales
-tol=1.e-02; %tolerancia para el eigenvalor con el valor minimo
-v=eig(H);   % vector de eigenvalores
+tol=1.e-02; %tolerancia para el eigenvalor mínimo
+v=eig(H); %eigenvalores de H
 [n,m]=size(H);
-vmin=min(v);
+vmin=min(v); %eigenvalor mínimo de H
 
+%Si el eigenvalor mínimo es muy pequeño, se arregla la matriz para que sea
+%spd
 if(vmin < tol)
-   H= H+ (abs(vmin)+1)*eye(n); %se le suma una constante a la diagonal de la matriz hessiana en caso de ser mal condicionada
-                              %para que sea bien condicionada
+   %Se le suma una constante a la diagonal de la matriz hessiana en caso de ser mal condicionada
+   %para que sea bien condicionada
+   H= H+ (abs(vmin)+1)*eye(n);
 end
 
 end
